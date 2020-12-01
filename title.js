3734515f6838d08b3276f16d488922a041cca373
User <user@Users-MacBook-Pro.local>
2020-12-02 09:55:28 +1000

    removed title page files as were unnecessary

--- title.js
+++
@@ -1,7 +0,0 @@
-const titlePageEl = document.querySelector(".titlePageContent");
-const titlePageButton = document.querySelector("#titlePageButton");
-
-titlePageButton.addEventListener("click", function () {
-    titlePageEl.classList.add("hide")
-
-});
