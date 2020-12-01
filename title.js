c5201bf8a460c0fae2396d6c4c36f22753628d31
User <user@Users-MacBook-Pro.local>
2020-12-01 11:20:03 +1000

    added images to repo and did some altercations to title page

---
+++ title.js
@@ -0,0 +1,7 @@
+const titlePageEl = document.querySelector(".titlePageContent");
+const titlePageButton = document.querySelector("#titlePageButton");
+
+titlePageButton.addEventListener("click", function () {
+    titlePageEl.classList.add("hide")
+
+});
